276abf75663a6acf646b9c0d3ba45b42e0dfed3a
Nathan Neidigh <Nathan.Neidigh@Outlook.com>
2025-12-11 01:35:12 -0800

    Update

--- data_logger.py
+++ data_logger.py
@@ -9,6 +9,11 @@ import matplotlib.pyplot as plt
 spi = spidev.SpiDev(0, 0)
 spi.max_speed_hz = 10_000_000 #10MHz
 spi.mode = 0b11
+WINDOW_SIZE = 100
+
+index = 0
+zs = np.zeros(WINDOW_SIZE, dtype=np.float64)
+times = np.zeros(WINDOW_SIZE, dtype=np.float64)
 
 def write_reg(reg, value):
     spi.xfer2([reg & 0x7F, value])
@@ -19,45 +24,45 @@ def read_reg(reg, msg_len):
     
     return data
 
-def read_accel():
-    msg_len = 6
-    data = read_reg(0x28, msg_len)
-    x, y, z = struct.unpack('<hhh', bytes(data[1:]))
-
-    g = 0.000061
-    return x*g, y*g, z*g
-
 def setup_IMU():
     write_reg(0x10, 0x60)
 
-setup_IMU()
+def read_accel():
+    data = read_reg(0x2C, 2)
+    z = int.from_bytes(data[1:], byteorder="little", signed=True)
+    g = 0.000061
+    return z*g
 
-xs, ys, zs = [], [], []
-times = []
+def update_buffers(times, zs, t, z):
+    global index
+    times[index] = t
+    zs[index] = z
+    index = (index + 1) % WINDOW_SIZE
+
+def read_buffer(buf):
+    global index
+    return np.concatenate((buf[index:], buf[:index]))
+
+setup_IMU()
 
 start = time.time()
 
 plt.ion()
 fig, ax = plt.subplots()
-line_x, = ax.plot([], [], label='Ax')
-line_y, = ax.plot([], [], label='Ay')
 line_z, = ax.plot([], [], label='Az')
-ax.legend()
-ax.set_ylim(-2, 2)
+ax.set_ylim(0.9, 1.1)
 ax.set_xlabel("Time (s)")
 ax.set_ylabel("Acceleration (g)")
 
 while True:
-    x, y, z = read_accel()
+    z = read_accel()
     t = time.time() - start
 
-    xs.append(x)
-    ys.append(y)
-    zs.append(z)
-    times.append(t)
+    update_buffers(times, zs, t, z)
 
-    line_x.set_data(times, xs)
-    line_y.set_data(times, ys)
-    line_z.set_data(times, zs)
+    line_z.set_data(read_buffer(times), read_buffer(zs))
     ax.set_xlim(max(0, t-5), t)
-    plt.pause(0.001)
+
+    sample_rate = WINDOW_SIZE  / (times[index-1] - times[index]) 
+    ax.set_title(f"Linear Acceleraion data at {sample_rate}")
+    plt.pause(0.0000001)
