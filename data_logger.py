303bd78c95d6e79d65a0b827c2e100293ef1c70e
Nathan Neidigh <nathan.neidigh@outlook.com>
2025-12-19 12:27:56 -0800

    Add conditional for fgpa execution or solo execution

--- data_logger.py
+++ data_logger.py
@@ -34,6 +34,12 @@ def read_accel():
     g = 0.000061
     return z*g
 
+def read_accel_fpga():
+    data = spi.xfer2([0]*2)
+    z = int.from_bytes(data, byteorder="big", signed=True)
+    g = 0.000061
+    return z*g
+
 def update_buffers(times, zs, t, z):
     global index
     times[index] = t
@@ -44,30 +50,57 @@ def read_buffer(buf):
     global index
     return np.concatenate((buf[index:], buf[:index]))
 
-setup_IMU()
+if (False):
+    setup_IMU()
 
-plt.ion()
-fig, ax = plt.subplots()
-line_z, = ax.plot([], [], label='Az')
-ax.set_ylim(0.9, 1.1)
-ax.set_xlabel("Time (s)")
-ax.set_ylabel("Acceleration (g)")
+    plt.ion()
+    fig, ax = plt.subplots()
+    line_z, = ax.plot([], [], label='Az')
+    ax.set_ylim(0.9, 1.1)
+    ax.set_xlabel("Time (s)")
+    ax.set_ylabel("Acceleration (g)")
 
-start = time.perf_counter()
-last_plot = time.perf_counter()
+    start = time.perf_counter()
+    last_plot = time.perf_counter()
 
-while True:
-    z = read_accel()
-    t = time.perf_counter() - start
-    update_buffers(times, zs, t, z)
+    while True:
+        z = read_accel()
+        t = time.perf_counter() - start
+        update_buffers(times, zs, t, z)
 
-    now = time.perf_counter()
-    if now - last_plot > PLOT_INTERVAL:
-        last_plot = now
-        valid_times = read_buffer(times)
-        valid_zs = read_buffer(zs)
-        line_z.set_data(valid_times, valid_zs)
-        ax.set_xlim(min(valid_times), max(valid_times))
-        sample_rate = WINDOW_SIZE  / (times[index-1] - times[index]) 
-        ax.set_title(f"Linear Acceleraion data at {sample_rate}")
-        plt.pause(0.001)
+        now = time.perf_counter()
+        if now - last_plot > PLOT_INTERVAL:
+            last_plot = now
+            valid_times = read_buffer(times)
+            valid_zs = read_buffer(zs)
+            line_z.set_data(valid_times, valid_zs)
+            ax.set_xlim(min(valid_times), max(valid_times))
+            sample_rate = WINDOW_SIZE  / (times[index-1] - times[index]) 
+            ax.set_title(f"Linear Acceleraion data at {sample_rate}")
+            plt.pause(0.001)
+else:
+    plt.ion()
+    fig, ax = plt.subplots()
+    line_z, = ax.plot([], [], label='Az')
+    ax.set_ylim(0.9, 1.1)
+    ax.set_xlabel("Time (s)")
+    ax.set_ylabel("Acceleration (g)")
+
+    start = time.perf_counter()
+    last_plot = time.perf_counter()
+
+    while True:
+        z = read_accel_fpga()
+        t = time.perf_counter() - start
+        update_buffers(times, zs, t, z)
+
+        now = time.perf_counter()
+        if now - last_plot > PLOT_INTERVAL:
+            last_plot = now
+            valid_times = read_buffer(times)
+            valid_zs = read_buffer(zs)
+            line_z.set_data(valid_times, valid_zs)
+            ax.set_xlim(min(valid_times), max(valid_times))
+            sample_rate = WINDOW_SIZE  / (times[index-1] - times[index]) 
+            ax.set_title(f"Linear Acceleraion data at {sample_rate}")
+            plt.pause(0.001)
